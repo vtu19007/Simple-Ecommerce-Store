5ac59af54a1920937b9a1b342bdfc6dcc1623031
kaviya Sri <146427552+vtu19007@users.noreply.github.com>
2024-07-15 21:25:21 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,8 @@
+# ecommerce_app/views.py
+
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
